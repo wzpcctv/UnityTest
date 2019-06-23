7864d699abee76c2d844ad2d379a069dd0ad83ee
wzpcctv <1335658675@qq.com>
2019-06-23 21:35:48 +0800

    更新

---
+++ test/Assets/Scripts/Unit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit
+{
+    //构造函数
+    public Unit()
+    {
+    }
+    public Unit(string name)
+    {
+        this.name = name;
+        Debug.Log("构造函数 - 单位创建 - " + this.name);
+    }
+
+    ~Unit()
+    {
+        Debug.Log("删除单位 - " + this.name);
+    }
+
+    static int unitNUm = 0;
+    private string name;
+}
